6d86f58a51c117c56e79f48588405226eacdaac0
jotoki3142 <shinaki3142@gmail.com>
2025-11-04 21:32:48 +0700

    Home Page

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-8 text-center text-sm text-zinc-500">
+      © 2025 QuizzZone. Mọi quyền được bảo lưu.
+    </footer>
+  );
+}
