40fbad88756c85e6a386634f00dafa3958adc98d
Mint <demonheart752005@gmail.com>
2025-12-16 21:30:14 +0700

    fix deploy 9

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,4 +1,8 @@
-interface CustomWindow extends Window {
-    firebase?: any;
+declare const __firebase_config: string;
 
+declare const __initial_auth_token: string;
+
+interface CustomWindow extends Window {
+    firebase: any;
+}
 declare var window: CustomWindow;
