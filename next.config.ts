33662a23f0a37f099a3cf1c5d39b609b137b7018
Mint <demonheart752005@gmail.com>
2025-12-16 23:20:09 +0700

    fix deploy 12

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://quizi.up.railway.app/api/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
